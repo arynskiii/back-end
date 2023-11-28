2375713c50d599251c93a904cd721ad5f94c5447
arynskiii <muratov_aryn@mail.ru>
2023-11-28 16:49:57 +0600

    init CI workflow

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -7,6 +7,10 @@ import (
 	"testing"
 )
 
+// sudo docker exec -it postgres12 psql --username=root --dbname=simple_bank
+// docker run --name postgres12 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -p 5432:5432 -d postgres:12-alpine
+// migrate -path db/migration -database "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose up
+// sudo service postgresql stop
 func createRandomAccount(t *testing.T) Account {
 	account := CreateAccountParams{
 		Owner:    util.RandomOwner(),
