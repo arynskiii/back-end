aeee6c271b29f8fdccf9ad3343511d15192cab8a
arynskiii <muratov_aryn@mail.ru>
2023-12-05 10:46:52 +0600

    update any func

--- main.go
+++ main.go
@@ -13,21 +13,25 @@ import (
 func main() {
 	cfg, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatalf("cannot load config: %v", err)
+		log.Println(err)
+		return
 	}
 
 	conn, err := sql.Open(cfg.DBDriver, cfg.DBSource)
 	if err != nil {
-		log.Fatal("cannot connect to db: %v", err)
+		log.Println(err)
+		return
 	}
 
 	store := db.NewStore(conn)
 	server, err := api.NewServer(cfg, store)
 	if err != nil {
-		log.Fatal("cannot create server: %v", err)
+		log.Println(err)
+		return
 	}
 
 	if err := server.Start(cfg.ServerAddress); err != nil {
-		log.Fatalf("cannot start server: %v", err)
+		log.Println(err)
+		return
 	}
 }
